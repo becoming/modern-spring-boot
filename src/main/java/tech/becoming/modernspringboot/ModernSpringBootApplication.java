246899ba228d627891c17861a021946c6853352b
Rodislav MOLDOVAN <rodislav@gmail.com>
2021-02-23 21:45:45 +0100

    added apm via code, this way the agent is located inside the app

--- src/main/java/tech/becoming/modernspringboot/ModernSpringBootApplication.java
+++ src/main/java/tech/becoming/modernspringboot/ModernSpringBootApplication.java
@@ -1,5 +1,6 @@
 package tech.becoming.modernspringboot;
 
+import co.elastic.apm.attach.ElasticApmAttacher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -8,6 +9,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class ModernSpringBootApplication {
 
 	public static void main(String[] args) {
+		ElasticApmAttacher.attach();
 		SpringApplication.run(ModernSpringBootApplication.class, args);
 	}
 

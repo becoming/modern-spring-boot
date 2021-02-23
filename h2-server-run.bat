99d7ddeb71402bfe7444429273e01318b860d1a2
Rodislav MOLDOVAN <rodislav@gmail.com>
2021-02-23 16:31:49 +0100

    exposing openly lots of information via health endpoint

---
+++ h2-server-run.bat
@@ -0,0 +1,1 @@
+java -cp C:\Users\%USER%\.m2\repository\com\h2database\h2\1.4.200\h2-1.4.200.jar org.h2.tools.Server -tcpPort 9003 -web -webAllowOthers -tcp -tcpAllowOthers -ifNotExists -baseDir C:\Users\rodi\.becoming

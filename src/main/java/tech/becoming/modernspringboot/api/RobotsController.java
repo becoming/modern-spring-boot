fd137af2a61ad8197517e3f974673ed8a178ea1f
Rodislav MOLDOVAN <rodislav@gmail.com>
2021-04-03 23:16:08 +0200

    working with vavr

--- src/main/java/tech/becoming/modernspringboot/api/RobotsController.java
+++ src/main/java/tech/becoming/modernspringboot/api/RobotsController.java
@@ -3,21 +3,21 @@ package tech.becoming.modernspringboot.api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import tech.becoming.modernspringboot.domain.RobotsService;
 import tech.becoming.modernspringboot.domain.dto.NewRobotRequest;
 import tech.becoming.modernspringboot.domain.dto.PatchRobotRequest;
-import tech.becoming.modernspringboot.domain.RobotsService;
 
 @RestController
 @RequestMapping("robots")
 @RequiredArgsConstructor
 public class RobotsController {
 
-    private final RobotsService service;
+    private final RobotsService    service;
     private final ControllerHelper helper;
 
     @GetMapping
     public ResponseEntity<String> getRobots(@RequestParam(defaultValue = "0") int page,
-                                                     @RequestParam(defaultValue = "50") int size) {
+                                            @RequestParam(defaultValue = "50") int size) {
         return service.findInRange(page, size).transform(helper::toJson);
     }
 
@@ -33,7 +33,7 @@ public class RobotsController {
 
     @PutMapping("{id}")
     public ResponseEntity<String> update(@PathVariable Long id,
-                            @RequestBody PatchRobotRequest dto) {
+                                         @RequestBody PatchRobotRequest dto) {
         return service.update(id, dto).transform(helper::toJson);
     }
 

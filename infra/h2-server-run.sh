605eb77b4b2613c818b59c38930d3b4988dc6a29
Rodislav MOLDOVAN <rodislav@gmail.com>
2022-06-25 19:33:12 +0200

    aligned to feature/classic-no-vavr

--- infra/h2-server-run.sh
+++ infra/h2-server-run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-java -cp /home/"$USER"/.m2/repository/com/h2database/h2/1.4.200/h2-1.4.200.jar \
+java -cp /home/"$USER"/.m2/repository/com/h2database/h2/2.1.212/h2-2.1.212.jar \
     org.h2.tools.Server \
     -tcpPort 9003 \
     -web -webAllowOthers \
